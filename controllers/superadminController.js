bcb7d28b3645f0da60daa33e525de965825c9436
nirav-minemagma <nirav@minemagma.com>
2024-07-26 22:12:58 +0530

    added

--- controllers/superadminController.js
+++ controllers/superadminController.js
@@ -1,38 +1,90 @@
-const db = require('../config/db');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const Superadmin = require('../models/Superadmin');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const Superadmin = {
-  create: (superadmin, callback) => {
-    const sql = 'INSERT INTO superadmins SET ?';
-    db.query(sql, superadmin, callback);
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  auth: {
+    user: "unizeinventiv@gmail.com",
+    pass: "HhPTI91mOfkWL4FC",
   },
-  update: (id, updatedData, callback) => {
-    const sql = 'UPDATE superadmins SET ? WHERE id = ?';
-    db.query(sql, [updatedData, id], callback);
-  },
-  getAll: (callback) => {
-    const sql = 'SELECT * FROM superadmins';
-    db.query(sql, callback);
-  },
-  findByEmail: (email, callback) => {
-    const sql = 'SELECT * FROM superadmins WHERE email = ?';
-    db.query(sql, [email], callback);
-  },
-  findByResetToken: (resetToken, callback) => {
-    const sql = 'SELECT * FROM superadmins WHERE reset_token = ? AND reset_token_expires > NOW()';
-    db.query(sql, [resetToken], callback);
-  },
-  updateResetToken: (email, resetToken, expiresIn, callback) => {
-    const sql = 'UPDATE superadmins SET reset_token = ?, reset_token_expires = ? WHERE email = ?';
-    db.query(sql, [resetToken, expiresIn, email], callback);
-  },
-  updatePassword: (id, password, callback) => {
-    const sql = 'UPDATE superadmins SET password = ? WHERE id = ?';
-    db.query(sql, [password, id], callback);
-  }
+});
+
+exports.requestPasswordReset = (req, res) => {
+  const { email } = req.body;
+
+  Superadmin.findByEmail(email, (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: "Error finding user", error: err });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "No user found with that email address" });
+    }
+
+    const user = results[0];
+    const resetToken = jwt.sign({ email }, "your_reset_password_secret", { expiresIn: '1h' });
+
+    Superadmin.updateResetToken(email, resetToken, Date.now() + parseInt(3600000), (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Error updating reset token", error: err });
+      }
+
+      const resetUrl = `https://advocate.unize.co.in/reset-password?token=${resetToken}`;
+      const mailOptions = {
+        to: email,
+        from: "unizeinventiv@gmail.com",
+        subject: 'Password Reset Request',
+        text: `To reset your password, click the following link: ${resetUrl}`,
+      };
+
+      transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+          return res.status(500).json({ message: "Error sending email", error: err });
+        }
+        res.status(200).json({ message: "Password reset email sent" });
+      });
+    });
+  });
 };
 
-module.exports = Superadmin;
+exports.resetPassword = (req, res) => {
+  const { token, newPassword } = req.body;
+
+  jwt.verify(token, "your_reset_password_secret", (err, decoded) => {
+    if (err) {
+      return res.status(400).json({ message: "Invalid or expired token", error: err });
+    }
+
+    Superadmin.findByResetToken(token, (err, results) => {
+      if (err) {
+        return res.status(500).json({ message: "Error finding user", error: err });
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ message: "Invalid or expired token" });
+      }
+
+      const user = results[0];
+      const hashedPassword = bcrypt.hashSync(newPassword, 8);
+
+      Superadmin.updatePassword(user.id, hashedPassword, (err) => {
+        if (err) {
+          return res.status(500).json({ message: "Error updating password", error: err });
+        }
+
+        Superadmin.updateResetToken(user.email, null, null, (err) => {
+          if (err) {
+            return res.status(500).json({ message: "Error clearing reset token", error: err });
+          }
+          res.status(200).json({ message: "Password updated successfully" });
+        });
+      });
+    });
+  });
+};
