7f4876be6e05e8d0b8851ca3a4a73bf24bf80993
Nirav Lathiya <83869023+Nirav591@users.noreply.github.com>
2024-08-07 14:47:38 +0530

    user

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,6 +26,7 @@ const register = async (req, res) => {
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
+
     // Create new user
     await User.create({ email, username, password: hashedPassword, type });
     res.status(201).json({ message: 'User registered successfully' });
@@ -36,28 +37,22 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  
+
   try {
     const [rows] = await User.findByEmail(email);
-    console.log('User query result:', rows);
-    
     if (!rows || rows.length === 0) {
       return res.status(400).json({ message: 'User not found' });
     }
-    
+
     const user = rows[0];
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log('Password match:', isMatch);
-    
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
-    
+
     const token = jwt.sign({ id: user.id, type: user.type }, secret, { expiresIn: '1h' });
-    console.log('Generated token:', token);
     res.json({ token });
   } catch (error) {
-    console.error('Error logging in:', error);
     res.status(500).json({ message: 'Error logging in', error });
   }
 };
