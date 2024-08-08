a617a03687050afbc72d0cef5ed445b36a605102
nirav-minemagma <nirav@minemagma.com>
2024-08-08 21:54:12 +0530

    added

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,9 +1,32 @@
 const pool = require('../config/db');
 
-const insertAdmin = async (adminData) => {
-  const [result] = await pool.query('INSERT INTO admins SET ?', adminData);
-  return result;
+// adminModel.js
+const insertAdmin = (data) => {
+  // Example using MySQL
+  return new Promise((resolve, reject) => {
+    const query = `INSERT INTO admins (admin_firstname, admin_lastname, admin_email_address, admin_phoneno, user_image, user_name, admin_password, status, added_by, date_added) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const values = [
+      data.admin_firstname,
+      data.admin_lastname,
+      data.admin_email_address,
+      data.admin_phoneno,
+      data.user_image,
+      data.user_name,
+      data.admin_password,
+      data.status,
+      data.added_by,
+      data.date_added,
+    ];
+
+    db.query(query, values, (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results);
+    });
+  });
 };
+
 
 const getAdminByDetails = async (admin_firstname, admin_lastname, admin_email_address, admin_phoneno, user_name) => {
   const [rows] = await pool.query(
