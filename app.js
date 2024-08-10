31d90d406586dba2da8a8375245e229b80865ced
nirav-minemagma <nirav@minemagma.com>
2024-08-10 22:07:27 +0530

    add

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-const fs = require('fs');
-const path = require('path');
-
-
 
 
 require('dotenv').config();
@@ -40,38 +36,12 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 
-
-
 // Route middlewares
 app.use('/api/auth', authRoutes);
 app.use('/admin', adminRoutes);
 
 
-app.post('/upload', (req, res) => {
-  const { imageBase64, fileName } = req.body;
 
-  if (!imageBase64 || !fileName) {
-      return res.status(400).json({ error: 'Image data and file name are required' });
-  }
-
-  // Decode Base64 image
-  const base64Data = imageBase64.replace(/^data:image\/\w+;base64,/, '');
-  const buffer = Buffer.from(base64Data, 'base64');
-
-  // Save the image to the filesystem
-  const filePath = path.join(__dirname, 'uploads', fileName);
-
-  fs.writeFile(filePath, buffer, (err) => {
-      if (err) {
-          return res.status(500).json({ error: 'Failed to save image' });
-      }
-      res.status(200).json({ message: 'Image uploaded successfully' });
-  });
-});
-
-if (!fs.existsSync(path.join(__dirname, 'uploads'))) {
-  fs.mkdirSync(path.join(__dirname, 'uploads'));
-}
 
 
 
--- utils/uploadImage.js
+++ utils/uploadImage.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+
 const getCurrentTimestamp = () => {
   return moment().format('YYYYMMDDHHmmss');
 };
