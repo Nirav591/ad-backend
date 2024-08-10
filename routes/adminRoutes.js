f8a9fee934db721adb5e6ca204fcc0170f8b9b92
nirav-minemagma <nirav@minemagma.com>
2024-08-10 21:34:07 +0530

    add

--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const validateAdminData = require('../middlewares/validateAdminData');
-
-
-router.post('/create',  validateAdminData, adminController.createAdmin);
-
-module.exports = router;
--- utils/responseHandler.js
+++
@@ -1,8 +0,0 @@
-const responseHandler = (res, statusCode, message, data = null) => {
-  res.status(statusCode).json({
-    message,
-    data
-  });
-};
-
-module.exports = responseHandler;
