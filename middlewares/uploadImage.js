eb825bd033844531a4ef7be5375f2501e51c36c2
nirav-minemagma <nirav@minemagma.com>
2024-08-10 12:37:57 +0530

    added

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,10 +1,9 @@
-const multer = require('multer');
 const path = require('path');
-
+const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../uploads/');
+    cb(null, path.join(__dirname, '../uploads/')); // Adjust path as necessary
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
